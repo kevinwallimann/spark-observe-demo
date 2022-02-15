5b778931b0a1c998fda3b30ee2d2421e75e63c1e
Kevin Wallimann <kevin.wallimann@absa.africa>
2022-02-15 11:10:38 +0100

    Update to Spark 3.2.1 and add list collect

--- src/test/scala/com/github/kevinwallimann/ObserveStreamingTest.scala
+++ src/test/scala/com/github/kevinwallimann/ObserveStreamingTest.scala
@@ -1,11 +1,15 @@
 package com.github.kevinwallimann
 
+import org.apache.spark.sql.{Column, Row}
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.StreamingQueryListener.{QueryProgressEvent, QueryStartedEvent, QueryTerminatedEvent}
 import org.apache.spark.sql.streaming.{OutputMode, StreamingQueryListener}
+import org.apache.spark.sql.types.{IntegerType, StructType}
 import org.scalatest.{FlatSpec, Matchers}
 import za.co.absa.commons.spark.SparkTestBase
+
 import java.util.UUID
 
 
@@ -18,12 +22,14 @@ class ObserveStreamingTest extends FlatSpec with Matchers with SparkTestBase {
 
         println(s"BatchId: ${event.progress.batchId}")
         Option(event.progress.observedMetrics.get("checkpoint1")).foreach(row => {
+          println(s"Checkpoint 1 listCollect: ${row.getAs[Seq[Int]]("listCollect")}")
           println(s"Checkpoint 1 rowCount: ${row.getAs[Long]("rowCount")}")
           println(s"Checkpoint 1 sum: ${row.getAs[Long]("sum")}")
           println(s"Checkpoint 1 sumAbs: ${row.getAs[Long]("sumAbs")}")
           println(s"Checkpoint 1 crc32: ${row.getAs[Long]("crc32")}")
         })
         Option(event.progress.observedMetrics.get("checkpoint2")).foreach(row => {
+          println(s"Checkpoint 1 listCollect: ${row.getAs[Seq[Int]]("listCollect")}")
           println(s"Checkpoint 2 rowCount: ${row.getAs[Long]("rowCount")}")
           println(s"Checkpoint 2 sum: ${row.getAs[Long]("sum")}")
           println(s"Checkpoint 2 sumAbs: ${row.getAs[Long]("sumAbs")}")
@@ -39,12 +45,17 @@ class ObserveStreamingTest extends FlatSpec with Matchers with SparkTestBase {
 
     val uuid = UUID.randomUUID().toString
 
-    import spark.implicits._
-    val input = MemoryStream[Int](42, spark.sqlContext)
+    val schemaCatalyst = new StructType()
+      .add("key", IntegerType, nullable = true)
+      .add("value", IntegerType, nullable = true)
+
+    val collectListDeterministic = new Column(CollectListDeterministic(col("key").expr).toAggregateExpression(isDistinct = false))
+    val input = MemoryStream[Row](42, spark.sqlContext)(RowEncoder(schemaCatalyst))
     val df = input.toDF().
       withColumn("crc32value", crc32(col("value").cast("String"))).
       observe("checkpoint1",
         count(lit(1)).as("rowCount"),
+        collectListDeterministic.as("listCollect"),
         // countDistinct(col("value")).as("distinctCount"), // distinct aggregates are not allowed
         sum(col("value")).as("sum"),
         sum(abs(col("value"))).as("sumAbs"),
@@ -53,6 +64,7 @@ class ObserveStreamingTest extends FlatSpec with Matchers with SparkTestBase {
       filter("value % 2 == 0").
       observe("checkpoint2",
         count(lit(1)).as("rowCount"),
+        collectListDeterministic.as("listCollect"),
         // countDistinct(col("value")).as("distinctCount"), // distinct aggregates are not allowed
         sum(col("value")).as("sum"),
         sum(abs(col("value"))).as("sumAbs"),
@@ -63,12 +75,18 @@ class ObserveStreamingTest extends FlatSpec with Matchers with SparkTestBase {
       outputMode(OutputMode.Append()).
       option("checkpointLocation", s"/tmp/out-streaming/$uuid/checkpoint-location")
     val query = df.start(s"/tmp/out-streaming/$uuid/bla")
-    input.addData((1 to 100).map(_ * -1))
+    input.addData(generateData(1, 100))
     query.processAllAvailable()
-    input.addData((101 to 300).map(_ * -1))
+    input.addData(generateData(101, 300))
     query.processAllAvailable()
-    input.addData((301 to 600).map(_ * -1))
+    input.addData(generateData(301, 600))
     query.processAllAvailable()
     query.stop()
+  }
+
+  private def generateData(start: Int, end: Int): Seq[Row] = {
+    val keys = (start to end).map(_ % 2)
+    val value = start to end
+    keys.zip(value).map(a => Row(a._1, a._2))
   }
 }
